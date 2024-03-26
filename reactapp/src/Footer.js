501bebd9bfd986bbbde8fbb61a93f3f2ebb4dee5
karoldziadkowiec <karol.dziadkkowiec@gmail.com>
2024-03-26 09:52:54 +0100

    frontend: home page finished

--- reactapp/src/Footer.js
+++ reactapp/src/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="border-top footer bg-dark" style={{ color: 'white', position: 'fixed', bottom: 0, width: '100%', zIndex: 100 }}>
             <div className="container py-3">
-                <center>&copy; 2024 - MedicalClinicApp</center>
+                <center>&copy; 2024 - Karol Dziadkowiec</center>
             </div>
         </footer>
     );
--- reactapp/src/components/Home.js
+++ reactapp/src/components/Home.js
@@ -1,14 +1,35 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 import '../App.css';
 import '../styles/Home.css';
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const moveToPatientsPage = () => {
+    navigate('/patients');
+  };
+
+  const moveToNewPatientPage = () => {
+    navigate('/new-patient');
+  };
 
   return (
     <div className="Home">
-      <h1>Home page</h1>
+      <h1><span className="medicalClinic">MedicalClinic</span></h1>
+      <img src={require('../img/logo.png')} alt="logo" className="logo" />
+      <h2>Welcome to MedicalClinic, your trusted destination for comprehensive patient data management.</h2>
+      <h4>With MedicalClinic, you can securely store and access patient records.</h4>
+      <h5>Our user-friendly interface and robust security measures ensure peace of mind while managing sensitive medical information.</h5>
+      <p></p>
+      <div className="button-container">
+        <Button variant="info" onClick={moveToPatientsPage}>Patients</Button>
+        <span className="sign"> | </span>
+        <Button variant="success" onClick={moveToNewPatientPage}>New Patient</Button>
+      </div>
     </div>
-  );
+  );  
 }
 
 export default Home;
